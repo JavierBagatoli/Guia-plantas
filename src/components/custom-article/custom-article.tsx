4da97510bba13936187111f07480d280eb24dab6
Javier Bagatoli <javi.bagatoli@gmail.com>
2025-06-12 16:25:46 -0300

    fix distribucion de vista

--- src/components/custom-article/custom-article.tsx
+++ src/components/custom-article/custom-article.tsx
@@ -7,22 +7,48 @@ interface articleData {
 
 function CustomArticle(props:articleData) {
   const {title, text} = props
+
+  let isH3: boolean = true;
+
+
+
   return (
     <>
       <div className='card'>
-        <h2>
-          {title}
-        </h2>
-        {text.map(element => {
-          if(element === ''){
-            return <hr></hr>
-          }
-          return <section>
-            <p>
-              {element}
-            </p>
+        <div>
+          <header className='card-head'>
+            <h2>
+              {title}
+            </h2>
+          </header>
+          <section className='card-body'>
+            {
+            text.map((element) => {
+
+                if(element === ''){
+                  isH3 = true;
+                  return <img className='garden-separator' src='./src/assets/separador.png'/>
+                }
+                if(isH3){
+                  isH3 = false
+                  return <section>
+                    <h3>
+                      {element}
+                    </h3>
+                  </section>
+                }
+                return <section>
+                  <p>
+                    {element}
+                  </p>
+                </section>
+
+              })
+            }
+              
           </section>
-        })}
+        </div>
+       
       </div>
     </>
   )
--- src/data.ts
+++ src/data.ts
@@ -2,6 +2,7 @@ export const dataCurso : {title: string, texto: string[]}[] = [
     {
         title: 'Tipos de Planta',
         texto: [
+            'Categorias',
             'Arbol: Tiene un tallo principal',
             'Arbusto tiene varios tallos leñosos (con corteza)',
             'Herbaceas: Un 0.80 a 1.00 metro',
@@ -27,6 +28,7 @@ export const dataCurso : {title: string, texto: string[]}[] = [
     {
         title: 'Drenaje',
         texto: [
+            'Cuidado',
             'Un buen drenaje permite oxigenar las plantas, el plato esta bueno pero puede ahogar la planta ya que el agua acumulada en el plato evita que haya oxigeno.',
             '',
             'Materiales:',
@@ -37,19 +39,19 @@ export const dataCurso : {title: string, texto: string[]}[] = [
     {
         title: 'Riego',
         texto: [
+            'Cuidado',
             'Puede haber acumulaciones con sales si viene de la llave, es recomendable dejar estar el agua por un dia ara liberar todo el cloro',
             '',
             '¿Cuando regar?',
             'Peso de la maceta, con agua pesa mas.',
-            '',
             'Es mejor que pasen sed antes que ahogarlas, cada tipo de planta indica el estres hidrico.',
-            '',
             'Se puede usar un palito de comida china, enterrarlo en la planta (sin hacer fuerza se puede dañar la planta) sacamos el palito y si el tercio inferior esta seco, hay que regar para plantas que requeiren poco, a la mitad para las plantas de riego regular y no encharcar si es una planta que requiere riego constante.',
             '',
             'Modos de riego:',
             'Riego directo: Hechar agua sobre la tierrra a un lado (derecha o izquierda) ver que el agua baje, luego repetir del otro lado.',
             'Prestar atencion que no se mojen las ojas o tallas, pueden aparecer hongos.',
             '',
+            'Tipos de riego',
             'Riego por capilaridad (Follaje delicado): La maceta tiene que tener huecos en la base, llenamos un cuenco hasta un tercio de la macena a regar, metemos la maceta y la dejamos de 2 a  3 horas',
             'Riego por inmersion: Sumergir la macena en agua, sin mojar los tallos.',
             'Lavado de flollage: Se pueed usar un atomizador, no hay que pasarse con el agua para evitar hongos.',
@@ -60,13 +62,16 @@ export const dataCurso : {title: string, texto: string[]}[] = [
     {
         title: 'Sustrato',
         texto: [
+            'A tener en cuenta',
             'Cada caso necesita ser compuesto con varios recursos, no solo uno solo. Tambien, la tierra no debe ser demasiado apretada o suelta',
         ]
     },
     {
         title: 'Abono',
         texto: [
-            'Cuando el sustrato baja hay que agregar abonos, es residuo de comida, papel o carton.',
+            'Esta compuesto por:',
+            'Son residuo de comida, papel o carton.',
+            'Cuando el sustrato baja hay que agregar abonos',
             'elemento seco',
             'elemento humedo',
             'elemento seco',
